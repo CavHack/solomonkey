e7ab1bc1139202eca2f692cf3a48f73f242bb86e
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-11-03 17:55:57 -0700

    Inline Append
    Split and vectorized string object, add a delimiter and keep concatenating according to need

--- source/planck/solomonDB/core/solomondb_utils.h
+++ source/planck/solomonDB/core/solomondb_utils.h
@@ -18,6 +18,52 @@ namespace DBUtils {
 
 }
 
+  inline unsigned int decode_hex(const std::string &s) {
+    unsigned int ret;
+   std:stringstream stm;
+    stm << std::hex << s;
+    stm >> ret;
+    return ret;
+
+}
+
+  inline std::string encode_str_hex(const std::string &str){
+    std::string result;
+    for (unsigned char c: str) result += encode_hex(c);
+    return result;
+
+}
+
+  inline void split(const std::string &s, char delim,
+		    std::vector<std::string> * elems) {
+
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim))
+      {
+	(*elems).push_back(item);
+	
+      }
+  }
+
+  inline std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, &elems);
+    return elems;
+
+}
+
+  inline std::string get_json_field(const std::string &json,
+				    const std::string &key) {
+
+    auto key_pos = json.find(key);
+    auto quote_sign_pos_1 = json.find('\"', key_pos + 1);
+    auto quote_sign_pos_2 = json.find('\"', quote_sign_pos_1)
+
+  }
+}
+
 
 
 
