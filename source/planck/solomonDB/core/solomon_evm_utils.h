bd0a0e60973bdd39d39de212043b85fd2e4bb9bb
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-11-09 01:30:41 -0800

    Constructor for inline string encoding
    to do: get block number and

--- source/planck/solomonDB/core/solomon_evm_utils.h
+++ source/planck/solomonDB/core/solomon_evm_utils.h
@@ -8,3 +8,39 @@
 #include <cassert>
 
 #include "solomondb_bb_utils.h"
+
+namespace DBUtils {
+  namespace EVMUtils {
+
+    enum class EVMType { Ethereum, Parity};
+
+    inline std::string encode_string(const std::string& str) {
+
+      std::string utf8_encoded = encode_str_hex(str);
+      std::string l = encode_hex(utf8_encoded.length() / 2);
+      std::string ret = left_padding_string(l) + right_padding_string(utf8_encoded);
+      return ret;
+
+
+}
+
+
+    unsigned int get_tip_block_number();
+
+    unsigned int get_txn_block_number();
+
+    void unlock_address(const std::string& endpoint, const std::string& address);
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
