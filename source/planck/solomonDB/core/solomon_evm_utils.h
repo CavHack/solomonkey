ee3c7211cc207f6d168d3e976eb3416257bbcb64
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-11-09 01:16:06 -0800

    Set up Ethereum Virtual Machine
    Check for utils dependencies

---
+++ source/planck/solomonDB/core/solomon_evm_utils.h
@@ -0,0 +1,10 @@
+#ifndef SOLOMONDB_EVM_UTILS_H
+#define SOLOMONDB_EVM_UTILS_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cassert>
+
+#include "solomondb_bb_utils.h"
