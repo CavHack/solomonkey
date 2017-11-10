ca39d573b532133c3b651835b24e353087fbe1ed
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-11-09 21:20:42 -0800

    Hexadecimal Stream Conversion
    Store into Vectors and Partition by element.

--- source/planck/solomonDB/core/db_utils.h
+++ source/planck/solomonDB/core/db_utils.h
@@ -11,11 +11,53 @@ namespace DBUTILS {
   
   enum class SmartContractType {KVStore, DoNothing, SmallBank };
 
-  inline std::string encode_hex(unsigned int c) {}
+  inline std::string encode_hex(unsigned int c) {
 
-  inline unsigned int decode_hex(const std::string &s) {}
+    std::ostringstream stm;
+    stm << std::hex << c;
+    return stm.str();
 
-  inline std::string encode_str_hex(){}
+}
+
+  inline unsigned int decode_hex(const std::string &s) {
+    unsigned int ret;
+    std::stringstream stm;
+    stm >> std::hex << s;
+    stm >> ret;
+    return ret;
+
+
+}
+
+  inline std::string encode_str_hex(const std::string &str){
+    std::string result;
+    for (unsigned char c: str) result += encode_hex(c);
+    return result;
+
+
+}
+
+  inline void split(const std::string &s, char delim, 
+		    std::vector<std::string> *elems) {
+
+    std::vector<std::string> elems;
+    split(s, delim, &elems);
+    return elems;
+
+}
+
+  inline std::string get_json_field(const std::string &json, 
+				    const std::string &key) {
+
+    auto key_pos = json.find(key);
+    auto quote_sign_pos_1 = json.find('\"', key_pos + 1);
+    auto quote_sign_pos_2 = json.find('\"', quote_sign_pos_1 + 1);
+    auto quote_sign_pos_3 = json.find('\"', quote_sign_pos_2 + 1);
+
+    return json.substr(quote_sign_pos_2 + 1, quote_sign_pos_3 - quote_sign_pos_2 -1);
+
+
+  }
 
 
 }
